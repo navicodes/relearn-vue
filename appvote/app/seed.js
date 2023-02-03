2b388fadcc3f6f7aae744da2b09001793b8c7861
navilindo <hassainjr@gmail.com>
2023-02-03 10:37:24 +0200

    seed added to window

--- appvote/app/seed.js
+++ appvote/app/seed.js
@@ -1,11 +1,16 @@
-const submissions = [
-    {
-        id : 1,
-        title : 'AMD grand office in Africa',
-        description : 'New AMD grand office has opened in the center of Innovation Hub Mtwara, Tanzania',
-        url : '#',
-        votes : 16,
-        avatar : '../public/images/avatars/256_1.png',
-        subimissionImage : 'bi bi-amd feed-image'
-    },
-]
+window.Seed = function(){
+    const submissions = [
+        {
+            id : 1,
+            title : 'AMD grand office in Africa',
+            description : 'New AMD grand office has opened in the center of Innovation Hub Mtwara, Tanzania',
+            url : '#',
+            votes : 16,
+            avatar : '../public/images/avatars/256_1.png',
+            subimissionImage : 'bi bi-amd feed-image'
+        },
+    ];
+    return {
+        submissions: submissions
+    };
+}
