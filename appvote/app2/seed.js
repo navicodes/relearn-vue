0a0d37b823d13af8b0227081a21c77ca3fc0532d
navilindo <hassainjr@gmail.com>
2023-02-11 23:15:46 +0200

    app2 - components

---
+++ appvote/app2/seed.js
@@ -0,0 +1,45 @@
+
+    window.Seed = (function(){
+        const submissions = [
+            {
+                id : 1,
+                title : 'AMD grand office in Africa',
+                description : 'New AMD grand office has opened in the center of Innovation Hub Mtwara, Tanzania',
+                url : '#',
+                votes : 16,
+                avatar : '../public/images/avatars/256_1.png',
+                submissionImage : 'bi bi-amd feed-image'
+            },
+            {
+                id : 2,
+                title : 'Google office in East Africa',
+                description : 'New Google has opened in the center of Innovation Hub Nairobi, Kenya',
+                url : '#',
+                votes : 17,
+                avatar : '../public/images/avatars/256_2.png',
+                submissionImage : 'bi bi-google feed-image'
+            },
+            {
+                id : 4,
+                title : 'Facebook representative in Egypt met',
+                description : 'Facebook application center at the core of Innovation, Tanzania',
+                url : '#',
+                votes : 14,
+                avatar : '../public/images/avatars/256_4.png',
+                submissionImage : 'bi bi-facebook feed-image'
+            },
+            {
+                id : 5,
+                title : 'Amazon: SmartHomes in East Africa',
+                description : 'Amazon has got a license to make smart homes in Kigali , Rwanda', 
+                url : '#',
+                votes : 19,
+                avatar : '../public/images/avatars/256_5.png',
+                submissionImage : 'bi bi-houses feed-image'
+            },
+        ];
+        // return {
+        //     submissions: submissions
+        // };
+        return { submissions: submissions };
+    }());
