5f31132e4155ef4b93053994a84b7e15966e6556
navilindo <hassainjr@gmail.com>
2023-02-11 08:21:09 +0200

    seed submissions

--- appvote/app/seed.js
+++ appvote/app/seed.js
@@ -11,13 +11,22 @@
                 submissionImage : 'bi bi-amd feed-image'
             },
             {
-                id : 1,
+                id : 2,
                 title : 'Google office in East Africa',
                 description : 'New Google has opened in the center of Innovation Hub Mtwara, Kenya',
                 url : '#',
                 votes : 17,
                 avatar : '../public/images/avatars/256_2.png',
                 submissionImage : 'bi bi-house feed-image'
+            },
+            {
+                id : 4,
+                title : 'Facebook representative in Egypt met',
+                description : 'New AMD grand office has opened in the center of Innovation Hub Mtwara, Tanzania',
+                url : '#',
+                votes : 14,
+                avatar : '../public/images/avatars/256_1.png',
+                submissionImage : 'bi bi-facebook feed-image'
             }
         ];
         // return {
