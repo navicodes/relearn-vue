2b80ccdb73e488cf160d4db6a6f68d405febdc20
navilindo <hassainjr@gmail.com>
2023-02-11 08:21:34 +0200

    smart homes

--- appvote/app/seed.js
+++ appvote/app/seed.js
@@ -13,21 +13,30 @@
             {
                 id : 2,
                 title : 'Google office in East Africa',
-                description : 'New Google has opened in the center of Innovation Hub Mtwara, Kenya',
+                description : 'New Google has opened in the center of Innovation Hub Nairobi, Kenya',
                 url : '#',
                 votes : 17,
                 avatar : '../public/images/avatars/256_2.png',
-                submissionImage : 'bi bi-house feed-image'
+                submissionImage : 'bi bi-google feed-image'
             },
             {
                 id : 4,
                 title : 'Facebook representative in Egypt met',
-                description : 'New AMD grand office has opened in the center of Innovation Hub Mtwara, Tanzania',
+                description : 'Facebook application center at the core of Innovation, Tanzania',
                 url : '#',
                 votes : 14,
-                avatar : '../public/images/avatars/256_1.png',
+                avatar : '../public/images/avatars/256_4.png',
                 submissionImage : 'bi bi-facebook feed-image'
-            }
+            },
+            {
+                id : 5,
+                title : 'Amazon: SmartHomes in East Africa',
+                description : 'Amazon has got a license to make smart homes in Kigali , Rwanda', 
+                url : '#',
+                votes : 10,
+                avatar : '../public/images/avatars/256_5.png',
+                submissionImage : 'bi bi-amazon feed-image'
+            },
         ];
         // return {
         //     submissions: submissions
